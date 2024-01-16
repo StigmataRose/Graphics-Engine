f138aeefb360d1e01aa5876f6cfcea083999be9e
unknown <skyler@typewriteraudio.com>
2024-01-15 18:01:33 -0800

    logo projection

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,6 +10,7 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -45,10 +46,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-           -0.5f, -0.5f,
-            0.5f, -0.5f,
-            0.5f, 0.5f,
-           -0.5f, 0.5f
+           -0.5f, -0.5f, 0.0f, 0.0f,
+            0.5f, -0.5f, 1.0f, 0.0f,
+            0.5f, 0.5f, 1.0f, 1.0f,
+           -0.5f, 0.5f, 0.0f, 1.0f
         };
 
         unsigned int indices[]{
@@ -56,11 +57,16 @@ int main(void)
             2,3,0
         };
 
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+        VertexBufferLayout layout;
+        // vertex coordinates
+        layout.Push<float>(2);
+        //texture coordinates
+        layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
         IndexBuffer ib(indices, 6);
@@ -69,6 +75,9 @@ int main(void)
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
      
+        Texture texture("res/textures/logo.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         vb.Unbind();
@@ -79,6 +88,9 @@ int main(void)
         float increment = 0.05f;
 
         Renderer renderer;
+
+        // Set the clear color (optional)
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
--- src/Shader.h
+++ src/Shader.h
@@ -15,7 +15,7 @@ private:
 	std::string m_Filepath;
 	unsigned int m_RendererID;
 	//caching for uniforms
-	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
 	Shader(const std::string& filepath);
 	~Shader();
@@ -25,10 +25,12 @@ public:
 
 
 	//Set uniforms
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
